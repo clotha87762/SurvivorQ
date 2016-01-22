1b3c0dd943578c508077ca13fa05aa351686ee82
kamisama366095 <d366095@gmail.com>
2016-01-22 11:55:00 +0800

    SVQ0122_v0.01

---
+++ Assets/Box/Scripts/BoxController.cs
@@ -0,0 +1,192 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class BoxController : MonoBehaviour {
+
+    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float rotateSpeed = 10.0f;
+    [SerializeField] private float jumpPower = 5.0f;
+    [SerializeField] private float groundCheckDistance = 0.1f;
+    [Range(0.0f, 1.0f)][SerializeField] private float runExtraSpeedScale = 0.5f;      // speed * (1 + runExtraSpeedScale)
+    [Range(0.0f, 1.0f)][SerializeField] private float notGroundedScale = 0.5f;
+    [Range(0.0f, 1.0f)][SerializeField] private float attackStateScale = 0.3f;
+    [SerializeField] private ParticleSystem deathEffect;
+    [SerializeField] private ParticleSystem spawnEffect;
+
+    private int DeathTimes = 0;
+
+    private Rigidbody mRigidbody;
+    private Collider mCollider;
+    private Animator mAnimator;
+    private Transform mGroundCheck;
+    private Collider mAttackRange;
+    private TriggerChecker mGroundTrigger;
+    private List<Renderer> mBodyRenderers = new List<Renderer>();
+  
+    private float deathTime = 2.0f;
+
+    private Vector3 jumpVelocity;
+    private bool mGrounded = true;
+
+    private bool mAttack = false;
+    private int mAttackCount = 1;
+    private bool attackHit = false;
+
+    private bool controlByPlayer;
+
+    void Awake()
+    {
+        if (transform.gameObject.GetComponent<UserInputController>()) controlByPlayer = true;
+        else controlByPlayer = false;
+    }
+
+	// Use this for initialization
+	void Start () {
+        mRigidbody = transform.GetComponent<Rigidbody>();
+        mCollider = transform.GetComponent<Collider>();
+        mAnimator = transform.GetComponent<Animator>();
+        
+        GameObject mainBody = transform.Find("Body").gameObject;
+        mBodyRenderers = new List<Renderer>(mainBody.transform.GetComponentsInChildren<Renderer>());
+
+        mGroundCheck = transform.Find("groundCheck").transform;
+        mAttackRange = transform.Find("attackRange").GetComponent<Collider>();
+        mGroundTrigger = transform.Find("groundCheckCollider").GetComponent<TriggerChecker>();
+	}
+
+    void Update()
+    {
+        GroundStateCheck();
+    }
+
+    public void Move(Vector3 mMove, bool mRunning)
+    {
+        //not moving, return
+        UpdateAnimator(mMove, mRunning);
+        //if (mMove.magnitude <= 0.0f) return;
+        if (mMove.magnitude > 1.0f) mMove.Normalize();
+
+        //transform.LookAt(transform.position + mMove);
+        //rotate character to move direction if not in attack mode
+        if (mMove.magnitude > 0.0f && !mAttack)
+        {
+			Vector3 lookRotationVector = transform.position + mMove - transform.position;
+			if(lookRotationVector.magnitude > 0.1f)
+			{
+				Quaternion targetRotation = Quaternion.LookRotation(lookRotationVector);
+				transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.fixedDeltaTime * rotateSpeed);
+			}
+        }
+
+        Vector3 originalVelocity = mRigidbody.velocity;
+        //OnAir move slower
+        if (!mGrounded) mMove = mMove * notGroundedScale;
+        //if (!mGrounded) mMove = mMove * notGroundedScale + mRigidbody.velocity * (1.0f - notGroundedScale);
+        //if (!mGrounded) return;
+        //OnAttack move slower
+        if (mAttack) mMove = mMove * attackStateScale;
+        float extraSpeed = (mRunning) ? 1.0f + runExtraSpeedScale : 1.0f; 
+        mRigidbody.velocity = mMove * speed * extraSpeed + new Vector3(0, originalVelocity.y, 0);
+        if (!mGrounded) mRigidbody.velocity += jumpVelocity * (1.0f-notGroundedScale);
+    }
+
+    public void Jump()
+    {
+        //jump if grounded
+        if (mGrounded)
+        {
+            mRigidbody.velocity = new Vector3(mRigidbody.velocity.x, jumpPower, mRigidbody.velocity.z);
+            jumpVelocity = new Vector3( mRigidbody.velocity.x, 0, mRigidbody.velocity.z);
+        }
+    }
+
+    public void UpdateAnimator(Vector3 move, bool mRunning)
+    {
+        //in attack
+        if (mAttack) mAnimator.Play("boxAttack00");
+        //in moving
+        if (move.magnitude > 0.0f && mGrounded)
+        {
+            mAnimator.SetBool("Walking", true);
+            if (mRunning) mAnimator.speed = 1.0f + runExtraSpeedScale;
+            else mAnimator.speed = 1.0f;
+        }
+        else
+        {
+            mAnimator.SetBool("Walking", false);
+            mAnimator.speed = 1.0f;
+        }
+    }
+    //check if character grounded, update mGrounded
+    void GroundStateCheck()
+    {
+        /*
+        RaycastHit hit;
+        if (Physics.Raycast(mGroundCheck.position + Vector3.up * 0.1f, Vector3.down, out hit, groundCheckDistance)) 
+            mGrounded = true;
+        else 
+            mGrounded = false;
+        */
+        mGrounded = mGroundTrigger.isGrounded();
+    }
+
+    public void Attack()
+    {
+        mAttack = true;
+    }
+
+    public void AttackCompleteEvent()
+    {
+        mAttack = false;
+    }
+
+    public void AttackHitEvent()
+    {
+        attackHit = true;
+    }
+
+    public void AttackLeaveEvent()
+    {
+        attackHit = false;
+    }
+
+    public void Death()
+    {
+        Debug.Log(" die." + DeathTimes);
+        DeathTimes++;
+        jumpVelocity = Vector3.zero;
+
+        //mBody.SetActive(false);
+        mRigidbody.constraints = RigidbodyConstraints.FreezePosition;
+        mRigidbody.velocity = Vector3.zero;
+        mRigidbody.angularVelocity = Vector3.zero;
+        SetRenderer(false);
+        Destroy(Instantiate(deathEffect.gameObject, transform.position, transform.rotation) as GameObject, deathEffect.startLifetime);
+        if (!controlByPlayer) Destroy(this);
+        else
+        {
+            transform.gameObject.GetComponent<UserInputController>().SetEnable(false);
+
+        }
+    }
+
+    public void SetRenderer(bool set)
+    {
+        foreach (Renderer element in mBodyRenderers)
+        {
+            element.enabled = set;
+        }
+    }
+
+    public void lerpShow()
+    {
+        foreach (Renderer element in mBodyRenderers)
+        {
+            element.material.DOFade(0.0f, 1.0f).From();
+        }
+    }
+}
---
+++ Assets/Box/Scripts/UserInputController.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(BoxController))]
+public class UserInputController : MonoBehaviour
+{
+
+    private BoxController mBox;
+    private Transform mCamera;
+    private Vector3 moveVector;
+    private bool enableControl = true;
+	private bool resettingCamera = false;
+	private Vector3 cameraFoward;
+	private Vector3 cameraRight;
+	private float tempH, tempV, h, v;
+	private bool keyH, keyV;
+	private bool keyHAxis, keyVAxis;
+	
+    void Start()
+    {
+        if (Camera.main != null)
+        {
+            mCamera = Camera.main.transform;
+        }
+        else Debug.Log("userinput_controller::No main camera");
+        mBox = transform.GetComponent<BoxController>();
+    }
+	
+	void Update()
+	{        
+		if (enableControl)
+        {
+			if(!resettingCamera && Input.GetButtonDown ("ResetCamera")) 
+			{
+				Debug.Log("resettingCamera:: ture");
+				resettingCamera = true;
+				cameraFoward = Vector3.Scale(mCamera.forward, new Vector3(1, 0, 1)).normalized;
+				cameraRight = Vector3.Scale(mCamera.right, new Vector3(1, 0, 1)).normalized;
+				keyHAxis = (h>0.0f);
+				keyVAxis = (v>0.0f);
+				keyH = Input.GetButton ("Horizontal");
+				keyV = Input.GetButton ("Vertical");
+			}
+        }
+	}
+	
+	
+    void FixedUpdate()
+    {
+        if (enableControl)
+        {
+            MoveInput();
+            AttackInput();
+        }
+    }
+
+    void MoveInput()
+    {
+        //jump if character is grounded and Jump key is pressed
+        if (Input.GetButton("Jump")) mBox.Jump(); 
+
+
+        ///////////////////////////////////
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+		bool tempkeyHAxis = (h>0.0f);
+		bool tempkeyVAxis = (v>0.0f);
+		//if(resettingCamera && (Mathf.Abs(h-tempH) > 0.1f || Mathf.Abs(v-tempV) > 0.1f)) 
+		if(resettingCamera && ( (keyH != Input.GetButton ("Horizontal") || keyV != Input.GetButton ("Vertical")) || 
+								(tempkeyHAxis != keyHAxis || tempkeyVAxis != keyVAxis)
+								) )
+		{
+			resettingCamera = false;
+			Debug.Log("resettingCamera:: false");
+			//h = 0.0f;
+			//v = 0.0f;
+		}
+
+        //calculate move vector and pass to boxcontroller
+        if (mCamera != null)
+        {
+            if(!resettingCamera)
+			{
+				cameraFoward = Vector3.Scale(mCamera.forward, new Vector3(1, 0, 1)).normalized;
+				cameraRight = Vector3.Scale(mCamera.right, new Vector3(1, 0, 1)).normalized;
+			}
+            moveVector = v * cameraFoward + h * cameraRight;
+        }
+        else moveVector = v * Vector3.forward + h * Vector3.right;
+        mBox.Move(moveVector, Input.GetKey(KeyCode.LeftShift));
+        
+        //////////////////////////////////
+    }
+
+    void AttackInput()
+    {
+        if (Input.GetMouseButton(0)) mBox.Attack();
+    }
+
+    public void SetEnable(bool set)
+    {
+        enableControl = set;
+    }
+}
